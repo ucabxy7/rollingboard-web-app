3f6789df487c1434d65034264cdb7e7acc243bf6
Xiaoxia Yang <z5474541@ad.unsw.edu.au>
2025-12-25 13:43:35 +0800

    eslint, prettier, husky init

---
+++ prettier.config.mjs
@@ -0,0 +1,8 @@
+const config = {
+  plugin: "prettier-plugin-tailwindcss",
+  tailwindFunctions: ["mergetailwindclasses"],
+  tabwith: 2,
+  printWidth: 80,
+  trailingCompma: "all",
+};
+export default config;
